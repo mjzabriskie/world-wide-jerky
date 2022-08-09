7637dbb5a597103c857e00f494ecdad6333c600c
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-08 20:34:11 -0600

    Created starter code for frontend

---
+++ client/src/utils/queries.js
@@ -0,0 +1,73 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_PRODUCTS = gql`
+  query thoughts($username: String) {
+    thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_PRODUCT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
