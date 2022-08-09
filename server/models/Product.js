37c9ac2fa59e130b1d4685cc933076fea5a65203
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-08 19:16:00 -0600

    Initial commit of project wireframe

---
+++ server/models/Product.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const productSchema = new Schema(
+  {
+    productName: {
+      type: String,
+      required: 'You must have a product name',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const Product = model('Product', productSchema);
+
+module.exports = Product;
