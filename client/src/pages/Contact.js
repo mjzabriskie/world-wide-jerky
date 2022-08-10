40fe23ce9e26a518ab9567da6598a1c1b92a3f7b
Will G <wgrout87@gmail.com>
2022-08-10 15:55:12 -0600

    added contact form

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import emailjs from "emailjs-com";
+
+function Contact() {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        // These environment variables require input from a free emailjs acocunt at emailjs.com
+        emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, e.target, process.env.REACT_APP_YOUR_PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return (
+        <div className="">
+            <div className="" />
+            <div className="">
+                <h2>Contact Me</h2>
+                <form className="" onSubmit={sendEmail} method="POST">
+                    <label htmlFor="name">Name</label>
+                    <input className="" type="text" name="name" required />
+                    <label htmlFor="email">Email Address</label>
+                    <input className="" type="email" name="email" required />
+                    <label htmlFor="message">Message</label>
+                    <textarea rows='8' className="" type="text" name="message" required />
+                    <button className="" type="submit">Send Email</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
