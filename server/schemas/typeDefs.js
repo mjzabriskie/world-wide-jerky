2737f2a7db6482ba7069452ed88782dcfac7d78a
Luis <luisriveros26@gmail.com>
2022-08-12 17:00:30 -0600

    created cart landing page at localhost:3000/cartpage, will work on stripe next

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,15 +9,28 @@ const typeDefs = gql`
     email: String
   }
   type Product {
-
+    _id: ID
+    name: String
+    description: String
+    image: String
+    price: String
+    stock: String
+    nutrition: [Nutrition]
+    ingredients: [String]
   }
   type Auth {
     token: ID!
     user: User
   }
+
+  type Checkout {
+    session: ID
+  }
+
   type Query {
     me: User
     user(username: String!): User
+    checkout(products: [ID]!): Checkout
   }
   type Mutation {
     login(email: String!, password: String!): Auth
