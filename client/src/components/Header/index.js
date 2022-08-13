76d07e957a63f200fe9244ffa1061a95fa6b2fe0
Richard Caleb Lawver <clawver30@gmail.com>
2022-08-13 13:59:53 -0600

    modals work

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 import Modal from 'react-bootstrap/Modal';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from "../../utils/mutations";
+import { LOGIN_USER, ADD_USER } from "../../utils/mutations";
 
 const Header = (props) => {
-  const [formState, setFormState] = useState({ email: "", password: "" });
-  const [login, { error }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({ username: "", email: "", password: "" });
+  const [login, { loginError }] = useMutation(LOGIN_USER);
+  const [addUser, { singupError }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -20,7 +21,7 @@ const Header = (props) => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+  const handleLoginFormSubmit = async (event) => {
     event.preventDefault();
 
     try {
@@ -41,15 +42,36 @@ const Header = (props) => {
     });
   };
 
+  const handleSignupFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };  
+
   const logout = event => {
     event.preventDefault();
     Auth.logout();
   }
+// login modal stuff
+  const [openLoginModal, setOpenLoginModal] = useState(false);
 
-  const [openModal, setOpenModal] = useState(false);
+  const handleLoginClose = () => setOpenLoginModal(false);
+  const handleLoginShow = () => setOpenLoginModal(true);
 
-  const handleClose = () => setOpenModal(false);
-  const handleShow = () => setOpenModal(true);
+// signup modal stuff
+
+  const [openSignupModal, setOpenSignupModal] = useState(false);
+
+  const handleSignupClose = () => setOpenSignupModal(false);
+  const handleSignupShow = () => setOpenSignupModal(true);
 
   return (
     <header className="d-flex flex-wrap justify-content-center w-100">
@@ -66,16 +88,17 @@ const Header = (props) => {
               </>
             ) : (
               <>
-                <button onClick={handleShow}>
+                {/* Login Modal */}
+                <button onClick={handleLoginShow}>
                   Login
                 </button>
 
-                <Modal show={openModal} onHide={handleClose}>
+                <Modal show={openLoginModal} onHide={handleLoginClose}>
                   <Modal.Header closeButton>
                     <Modal.Title>Login</Modal.Title>
                   </Modal.Header>
                   <Modal.Body>
-                  <form onSubmit={handleFormSubmit}>
+                  <form onSubmit={handleLoginFormSubmit}>
                     <input
                       className="form-input"
                       placeholder="Your email"
@@ -98,7 +121,53 @@ const Header = (props) => {
                       Submit
                     </button>
                   </form>
-                  {error && <div>Login failed</div>}
+                  {loginError && <div>Login failed</div>}
+                  </Modal.Body>
+                </Modal>
+
+                {/* Signup Modal */}
+                <button onClick={handleSignupShow}>
+                  Sign Up
+                </button>
+
+                <Modal show={openSignupModal} onHide={handleSignupClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Sign Up</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                  <form onSubmit={handleSignupFormSubmit}>
+                    <input
+                      className="form-input"
+                      placeholder="Your username"
+                      name="username"
+                      type="username"
+                      id="username"
+                      value={formState.username}
+                      onChange={handleChange}
+                    />
+                    <input
+                      className="form-input"
+                      placeholder="Your email"
+                      name="email"
+                      type="email"
+                      id="email"
+                      value={formState.email}
+                      onChange={handleChange}
+                    />
+                    <input
+                      className="form-input"
+                      placeholder="******"
+                      name="password"
+                      type="password"
+                      id="password"
+                      value={formState.password}
+                      onChange={handleChange}
+                    />
+                    <button className="btn d-block w-100" type="submit">
+                      Submit
+                    </button>
+                  </form>
+                  {singupError && <div>Sign up failed</div>}
                   </Modal.Body>
                 </Modal>
                 {/* <Link className="text-decoration-none px-2" to="/login">Login</Link> */}
