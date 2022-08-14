bb9f4cb321cd74593c2b5b372c1d49c9e185049c
Luis <luisriveros26@gmail.com>
2022-08-14 09:41:47 -0600

    added to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
-// const { User, Thought } = require("../models"); Our models here
+const { User, Product, Order } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 
 const resolvers = {
   Query: {
