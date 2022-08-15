91aa8f256f0ced0b961f05233360b828c343aeee
Will G <wgrout87@gmail.com>
2022-08-15 17:14:01 -0600

    updated carousel image fit

--- client/src/components/CarouselImage/index.js
+++ client/src/components/CarouselImage/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import "./style.css";
 
 const CarouselImage = ({ name, description, image }) => {
     return (
         <>
-            <img className="d-block w-100" src={image[0]} alt={name} />
+            <img className="d-block w-100 carousel-image" src={image[0]} alt={name} />
             <Carousel.Caption>
                 <h3>{name}</h3>
                 <p>{description}</p>
